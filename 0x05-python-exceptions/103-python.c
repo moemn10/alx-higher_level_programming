cbfd60bf17896567dcaf0a7871b287bbf6a88456
unknown <moemnkoko@gmail.com>
2023-08-30 02:32:58 +0300

    moemn

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,6 +1,6 @@
 /*
  * File: 103-python.c
- * Auth: Moamen
+ * Auth: Type Your Name Here
  */
 
 #include <Python.h>
@@ -26,7 +26,7 @@ void print_python_list(PyObject *p)
 	fflush(stdout);
 
 	printf("[*] Python list info\n");
-	if (strcmp(p->ob_type->Moamen, "list") != 0)
+	if (strcmp(p->ob_type->tp_name, "list") != 0)
 	{
 		printf("  [ERROR] Invalid List Object\n");
 		return;
@@ -37,7 +37,7 @@ void print_python_list(PyObject *p)
 
 	for (i = 0; i < size; i++)
 	{
-		type = list->ob_item[i]->ob_type->Moamen;
+		type = list->ob_item[i]->ob_type->tp_name;
 		printf("Element %ld: %s\n", i, type);
 		if (strcmp(type, "bytes") == 0)
 			print_python_bytes(list->ob_item[i]);
@@ -58,7 +58,7 @@ void print_python_bytes(PyObject *p)
 	fflush(stdout);
 
 	printf("[.] bytes object info\n");
-	if (strcmp(p->ob_type->Moamen, "bytes") != 0)
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
@@ -96,7 +96,7 @@ void print_python_float(PyObject *p)
 	fflush(stdout);
 
 	printf("[.] float object info\n");
-	if (strcmp(p->ob_type->Moamen, "float") != 0)
+	if (strcmp(p->ob_type->tp_name, "float") != 0)
 	{
 		printf("  [ERROR] Invalid Float Object\n");
 		return;
